e37846be9cfdbe04523b854d6f47411ae66f47f3
Rémy MEJA <rmeja@issn.org>
2020-03-18 11:27:30 +0100

    update unit test

--- test/schema-test.js
+++ test/schema-test.js
@@ -9,73 +9,187 @@ const ajv = new Ajv({ allErrors: true });
 const validate = ajv.compile(schema);
 
 describe('json schema himarc', function () {
-  it('should validate leader', function () {
-    const data = {
-      fields: {
-        LDR: {
-          positions: {
-            10: '2',
-            11: '2',
-            17: ' ',
-            18: 'a',
-            19: ' ',
-            20: '4',
-            21: '5',
-            22: '0',
-            '00-04': '02105',
-            '05': 'c',
-            '06': 'a',
-            '07': 's',
-            '08': ' ',
-            '09': 'a',
-            '12-16': '00541'
-          }
-        }
-      }
-    };
-    const valid = validate(data);
-    expect(valid).to.be.true;
-  });
-
-  it('should validate 007 field', function () {
-    const data = {
-      fields: {
-        '007': {
-          positions: {
-            '00': 't',
-            '01': 'a'
-          }
-        }
-      }
-    };
-    const valid = validate(data);
-    expect(valid).to.be.true;
-  });
-
-  it('should validate 022 field', function () {
-    const data = {
-      fields: {
-        '022': [{
-          indicator1: '0',
-          indicator2: '\\',
-          subFields: [
-            {
-              a: '0028-0836'
-            },
-            {
-              z: '0302-2889'
-            },
-            {
-              2: '_2'
-            },
-            {
-              l: '0028-0836'
+  describe('LDR', function () {
+    it('should validate', function () {
+      const data = {
+        fields: {
+          LDR: {
+            positions: {
+              10: '2',
+              11: '2',
+              17: ' ',
+              18: 'a',
+              19: ' ',
+              20: '4',
+              21: '5',
+              22: '0',
+              '00-04': '02105',
+              '05': 'c',
+              '06': 'a',
+              '07': 's',
+              '08': ' ',
+              '09': 'a',
+              '12-16': '00541'
             }
-          ]
-        }]
-      }
-    };
-    const valid = validate(data);
-    expect(valid).to.be.true;
+          }
+        }
+      };
+      const valid = validate(data);
+      expect(valid).to.be.true;
+    });
+
+    it('shouldn\'t validate', function () {
+      const data = {
+        fields: {
+          LDR: {
+            positions: {
+              100: '2',
+              110: '2',
+              170: ' ',
+              18: 'a',
+              19: ' ',
+              20: '4',
+              21: '5',
+              22: '0',
+              '00-04': '02105',
+              '05': 'c',
+              '06': 'a',
+              '07': 's',
+              '08': ' ',
+              '09': 'a',
+              '12-16': '00541'
+            }
+          }
+        }
+      };
+      const valid = validate(data);
+      expect(valid).to.be.false;
+    });
+  });
+
+  describe('007', function () {
+    it('should validate', function () {
+      const data = {
+        fields: {
+          '007': {
+            positions: {
+              '00': 't',
+              '01': 'a'
+            }
+          }
+        }
+      };
+      const valid = validate(data);
+      expect(valid).to.be.true;
+    });
+
+    it('shouldn\'t validate', function () {
+      const data = {
+        fields: {
+          '007': {
+            positions: {
+              '00': 'x',
+              '01': 'a'
+            }
+          }
+        }
+      };
+      const valid = validate(data);
+      expect(valid).to.be.false;
+    });
+  });
+
+  describe('022', function () {
+    it('should validate', function () {
+      const data = {
+        fields: {
+          '022': [{
+            indicator1: '0',
+            indicator2: '\\',
+            subFields: [
+              {
+                a: '0028-0836'
+              },
+              {
+                z: '0302-2889'
+              },
+              {
+                2: '_2'
+              },
+              {
+                l: '0028-0836'
+              }
+            ]
+          }]
+        }
+      };
+      const valid = validate(data);
+      expect(valid).to.be.true;
+    });
+
+    it('shouldn\'t validate', function () {
+      const data = {
+        fields: {
+          '022': [{
+            indicator1: '5',
+            indicator2: '\\',
+            subFields: [
+              {
+                x: '0028-0836'
+              },
+              {
+                z: '0302-2889'
+              },
+              {
+                2: '_2'
+              },
+              {
+                l: '0028-0836'
+              }
+            ]
+          }]
+        }
+      };
+      const valid = validate(data);
+      expect(valid).to.be.false;
+    });
+  });
+
+  describe('044', function () {
+    it('should validate', function () {
+      const data = {
+        fields: {
+          '044': {
+            indicator1: '\\',
+            indicator2: '\\',
+            subFields: [
+              {
+                c: 'GBR'
+              }
+            ]
+          }
+        }
+      };
+      const valid = validate(data);
+      expect(valid).to.be.true;
+    });
+
+    it('shouldn\'t validate', function () {
+      const data = {
+        fields: {
+          '044': {
+            indicator1: '\\',
+            indicator2: '\\',
+            subFields: [
+              {
+                c: 'GBRA'
+              }
+            ]
+          }
+        }
+      };
+      const valid = validate(data);
+      expect(valid).to.be.false;
+    });
   });
 });
