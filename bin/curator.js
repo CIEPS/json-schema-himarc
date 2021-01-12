a2b1bb6bf53316cbaa4e74d6a451156a02b3b1fb
Rémy MEJA <rmeja@issn.org>
2021-01-12 10:55:14 +0100

    add curator script

---
+++ bin/curator.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const schema = require('../src');
+
+// add minItems to one for array field
+// Object.entries(schema)
+//   .filter(([key]) => key.startsWith('field') && !key.startsWith('field_00'))
+//   .filter(([key, value]) => value.type === 'array')
+//   .map(([key, value]) => {
+//     // console.log(key, value);
+//     value.minItems = 1;
+//     fs.writeFileSync(path.join(__dirname, `../src/${key.replace('_', '-')}.schema.json`), JSON.stringify(value, null, 2));
+//     return ([key, value]);
+//   });
+
+// add minItems to subfields array
+Object.entries(schema)
+  .filter(([key]) => key.startsWith('field') && !key.startsWith('field_00'))
+  .map(([key, value]) => {
+    if (value.type === 'object') {
+      if ('subFields' in value.properties) {
+        value.properties.subFields.minItems = 1;
+      }
+    }
+
+    if (value.type === 'array') {
+      if ('subFields' in value.items.properties) {
+        value.items.properties.subFields.minItems = 1;
+      }
+    }
+    fs.writeFileSync(path.join(__dirname, `../src/${key.replace('_', '-')}.schema.json`), JSON.stringify(value, null, 2));
+    return ([key, value]);
+  });
