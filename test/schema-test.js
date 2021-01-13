98292a44d50412d399e3baa5c47d1e013ff8b5ef
Rémy MEJA <rmeja@issn.org>
2021-01-13 14:25:05 +0100

    update unit test

--- test/schema-test.js
+++ test/schema-test.js
@@ -5,12 +5,7 @@
 const { expect } = require('chai');
 const Ajv = require('ajv');
 const refParser = require('@apidevtools/json-schema-ref-parser');
-const leaderSchema = require('../src/leader.schema.json');
-const field007Schema = require('../src/field-007.schema.json');
-const field008Schema = require('../src/field-008.schema.json');
-const field022Schema = require('../src/field-022.schema.json');
-const field044Schema = require('../src/field-044.schema.json');
-const field222Schema = require('../src/field-222.schema.json');
+const schemaHelper = require('../src');
 const fullSchema = require('../dist/himarc.schema.json');
 const schemaProxies = require('../main.js');
 
@@ -42,7 +37,7 @@ describe('LDR schema', function () {
       }
     };
     const ajv = new Ajv({ allErrors: true });
-    const validate = ajv.compile(leaderSchema);
+    const validate = ajv.compile(schemaHelper.leader);
     const valid = validate(data);
     expect(valid).to.be.true;
   });
@@ -68,7 +63,7 @@ describe('LDR schema', function () {
       }
     };
     const ajv = new Ajv({ allErrors: true });
-    const validate = ajv.compile(leaderSchema);
+    const validate = ajv.compile(schemaHelper.leader);
     const valid = validate(data);
     expect(valid).to.be.false;
   });
@@ -83,7 +78,7 @@ describe('007 schema', function () {
       }
     }];
     const ajv = new Ajv({ allErrors: true });
-    const validate = ajv.compile(field007Schema);
+    const validate = ajv.compile(schemaHelper.field_007);
     const valid = validate(data);
     expect(valid).to.be.true;
   });
@@ -96,7 +91,7 @@ describe('007 schema', function () {
       }
     }];
     const ajv = new Ajv({ allErrors: true });
-    const validate = ajv.compile(field007Schema);
+    const validate = ajv.compile(schemaHelper.field_007);
     const valid = validate(data);
     expect(valid).to.be.false;
   });
@@ -127,7 +122,7 @@ describe('008 schema', function () {
         '35-37': 'eng'
       }
     };
-    refParser.dereference(field008Schema, (error, schema) => {
+    refParser.dereference(schemaHelper.field_008, (error, schema) => {
       if (error) return done(error);
       const ajv = new Ajv({ allErrors: true });
       const validate = ajv.compile(schema);
@@ -168,7 +163,7 @@ describe('008 schema', function () {
         '35-37': 'eng'
       }
     };
-    refParser.dereference(field008Schema, (error, schema) => {
+    refParser.dereference(schemaHelper.field_008, (error, schema) => {
       if (error) return done(error);
       const ajv = new Ajv({ allErrors: true });
       const validate = ajv.compile(schema);
@@ -200,7 +195,7 @@ describe('022 schema', function () {
       ]
     }];
     const ajv = new Ajv({ allErrors: true });
-    const validate = ajv.compile(field022Schema);
+    const validate = ajv.compile(schemaHelper.field_022);
     const valid = validate(data);
     if (validate.errors) console.dir(validate.errors, { depth: 8 });
     expect(valid).to.be.true;
@@ -226,7 +221,7 @@ describe('022 schema', function () {
       ]
     }];
     const ajv = new Ajv({ allErrors: true });
-    const validate = ajv.compile(field022Schema);
+    const validate = ajv.compile(schemaHelper.field_022);
     const valid = validate(data);
     expect(valid).to.be.false;
   });
@@ -244,7 +239,7 @@ describe('044 schema', function () {
       ]
     };
     const ajv = new Ajv({ allErrors: true });
-    const validate = ajv.compile(field044Schema);
+    const validate = ajv.compile(schemaHelper.field_044);
     const valid = validate(data);
     if (validate.errors) console.dir(validate.errors, { depth: 8 });
     expect(valid).to.be.true;
@@ -261,7 +256,7 @@ describe('044 schema', function () {
       ]
     };
     const ajv = new Ajv({ allErrors: true });
-    const validate = ajv.compile(field044Schema);
+    const validate = ajv.compile(schemaHelper.field_044);
     const valid = validate(data);
     expect(valid).to.be.false;
   });
@@ -282,7 +277,7 @@ describe('222 schema', function () {
       ]
     }];
     const ajv = new Ajv({ allErrors: true });
-    const validate = ajv.compile(field222Schema);
+    const validate = ajv.compile(schemaHelper.field_222);
     const valid = validate(data);
     expect(valid).to.be.true;
   });
@@ -301,7 +296,7 @@ describe('222 schema', function () {
       ]
     }];
     const ajv = new Ajv({ allErrors: true });
-    const validate = ajv.compile(field222Schema);
+    const validate = ajv.compile(schemaHelper.field_222);
     const valid = validate(data);
     expect(valid).to.be.false;
   });
@@ -408,5 +403,196 @@ describe('full schema', function () {
     const valid = validate(data);
     if (validate.errors) console.dir(validate.errors, { depth: 8 });
     expect(valid).to.be.true;
+  });
+
+  it('shouldn\'t validate whith an empty subfield', function () {
+    const data = {
+      fields: {
+        LDR: {
+          positions: {
+            10: '2',
+            11: '2',
+            17: ' ',
+            18: 'a',
+            19: ' ',
+            20: '4',
+            21: '5',
+            22: '0',
+            '00-04': '02105',
+            '05': 'c',
+            '06': 'a',
+            '07': 's',
+            '08': ' ',
+            '09': 'a',
+            '12-16': '00541'
+          }
+        },
+        '008': {
+          positions: {
+            18: 'w',
+            19: 'r',
+            21: 'p',
+            22: ' ',
+            23: ' ',
+            24: ' ',
+            28: ' ',
+            29: '0',
+            33: 'a',
+            34: '0',
+            38: ' ',
+            39: ' ',
+            '00-05': '190816',
+            '06': 'c',
+            '07-10': '1869',
+            '11-14': '9999',
+            '15-17': 'enk',
+            '25-27': '   ',
+            '35-37': 'eng'
+          }
+        },
+        '022': [{
+          indicator1: '0',
+          indicator2: '\\',
+          subFields: [
+            {
+              a: '0028-0836'
+            },
+            {
+              z: '0302-2889'
+            },
+            {
+              2: '_2'
+            },
+            {
+              l: '0028-0836'
+            }
+          ]
+        }],
+        '044': {
+          indicator1: '\\',
+          indicator2: '\\',
+          subFields: []
+        },
+        '080': [{
+          indicator1: '0',
+          indicator2: '\\',
+          subFields: [
+            {
+              a: '539.120.222'
+            }
+          ]
+        }],
+        222: [{
+          indicator1: '\\',
+          indicator2: '0',
+          subFields: [
+            {
+              a: 'Nature'
+            },
+            {
+              b: '(London)'
+            }
+          ]
+        }]
+      }
+    };
+    const ajv = new Ajv({ allErrors: true });
+    const validate = ajv.compile(fullSchema);
+    const valid = validate(data);
+    if (validate.errors) {
+      expect(validate.errors.some(error => error.message === 'should NOT have fewer than 1 items')).to.be.true;
+    }
+    expect(valid).to.be.false;
+  });
+
+  it('shouldn\'t validate whith an empty tag', function () {
+    const data = {
+      fields: {
+        LDR: {
+          positions: {
+            10: '2',
+            11: '2',
+            17: ' ',
+            18: 'a',
+            19: ' ',
+            20: '4',
+            21: '5',
+            22: '0',
+            '00-04': '02105',
+            '05': 'c',
+            '06': 'a',
+            '07': 's',
+            '08': ' ',
+            '09': 'a',
+            '12-16': '00541'
+          }
+        },
+        '008': {
+          positions: {
+            18: 'w',
+            19: 'r',
+            21: 'p',
+            22: ' ',
+            23: ' ',
+            24: ' ',
+            28: ' ',
+            29: '0',
+            33: 'a',
+            34: '0',
+            38: ' ',
+            39: ' ',
+            '00-05': '190816',
+            '06': 'c',
+            '07-10': '1869',
+            '11-14': '9999',
+            '15-17': 'enk',
+            '25-27': '   ',
+            '35-37': 'eng'
+          }
+        },
+        '022': [{
+          indicator1: '0',
+          indicator2: '\\',
+          subFields: [
+            {
+              a: '0028-0836'
+            },
+            {
+              z: '0302-2889'
+            },
+            {
+              2: '_2'
+            },
+            {
+              l: '0028-0836'
+            }
+          ]
+        }],
+        '044': {},
+        '080': [],
+        222: [{
+          indicator1: '\\',
+          indicator2: '0',
+          subFields: [
+            {
+              a: 'Nature'
+            },
+            {
+              b: '(London)'
+            }
+          ]
+        }]
+      }
+    };
+    const ajv = new Ajv({ allErrors: true });
+    const validate = ajv.compile(fullSchema);
+    const valid = validate(data);
+    if (validate.errors) {
+      expect(validate.errors.some(error => error.message === "should have required property 'indicator1'")).to.be.true;
+      expect(validate.errors.some(error => error.message === "should have required property 'indicator2'")).to.be.true;
+      expect(validate.errors.some(error => error.message === "should have required property 'subFields'")).to.be.true;
+      expect(validate.errors.some(error => error.message === 'should NOT have fewer than 1 items')).to.be.true;
+    }
+    expect(valid).to.be.false;
   });
 });
