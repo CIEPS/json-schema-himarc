a5c7b090ada2631d1fc98c55dead1a382cd47a26
Rémy MEJA <rmeja@issn.org>
2021-01-15 16:08:57 +0100

    fix isRepeatable property

--- bin/curator.js
+++ bin/curator.js
@@ -14,20 +14,30 @@ const schema = require('../src');
 //   });
 
 // add minItems to subfields array
-Object.entries(schema)
-  .filter(([key]) => key.startsWith('field') && !key.startsWith('field_00'))
-  .map(([key, value]) => {
-    if (value.type === 'object') {
-      if ('subFields' in value.properties) {
-        value.properties.subFields.minItems = 1;
-      }
-    }
+// Object.entries(schema)
+//   .filter(([key]) => key.startsWith('field') && !key.startsWith('field_00'))
+//   .map(([key, value]) => {
+//     if (value.type === 'object') {
+//       if ('subFields' in value.properties) {
+//         value.properties.subFields.minItems = 1;
+//       }
+//     }
 
-    if (value.type === 'array') {
-      if ('subFields' in value.items.properties) {
-        value.items.properties.subFields.minItems = 1;
-      }
-    }
+//     if (value.type === 'array') {
+//       if ('subFields' in value.items.properties) {
+//         value.items.properties.subFields.minItems = 1;
+//       }
+//     }
+//     fs.writeFileSync(path.join(__dirname, `../src/${key.replace('_', '-')}.schema.json`), JSON.stringify(value, null, 2));
+//     return ([key, value]);
+//   });
+
+// fix isRepeatable
+Object.entries(schema)
+  .filter(([key, value]) => value.type === 'array')
+  .map(([key, value]) => {
+    if ('isRepeatable' in value.items) delete value.items.isRepeatable;
+    if ('isRepeatable' in value === false) value.isRepeatable = true;
     fs.writeFileSync(path.join(__dirname, `../src/${key.replace('_', '-')}.schema.json`), JSON.stringify(value, null, 2));
     return ([key, value]);
   });
