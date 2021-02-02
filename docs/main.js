ef5f5c07d4ac40c06ecb3c7bde3540425174a3e0
Rémy MEJA <rmeja@issn.org>
2021-02-02 21:21:38 +0100

    update

--- docs/main.js
+++ docs/main.js
@@ -5,9 +5,17 @@ fetch('https://raw.githubusercontent.com/CIEPS/json-schema-himarc/add-profil-iss
     return response.json();
   })
   .then(function (data) {
+    const controlfields = Object.entries(data.properties.fields.properties)
+      .filter(([tag]) => ['001', '003', '005'].includes(tag))
+      .sort((a, b) => a[0] - b[0])
+      .map(([tag, value]) => {
+        const output = (value.type === 'array')
+          ? { tag, value: { ...value, ...value.items } }
+          : { tag, value };
+        return output;
+      });
     const datafields = Object.entries(data.properties.fields.properties)
       .filter(([tag]) => !tag.startsWith('00') && tag !== 'LDR')
-      // .sort((a, b) => (a[0] === 'LDR' ? -Infinity : b[0] === 'LDR' ? Infinity : a[0] - b[0]))
       .sort((a, b) => a[0] - b[0])
       .map(([tag, value]) => {
         const output = (value.type === 'array')
@@ -17,7 +25,7 @@ fetch('https://raw.githubusercontent.com/CIEPS/json-schema-himarc/add-profil-iss
       });
     const source = document.getElementById('app-template').innerHTML;
     const template = Handlebars.compile(source);
-    const context = { datafields };
+    const context = { datafields, controlfields };
     const html = template(context);
     document.getElementById('app').innerHTML = html;
   });
