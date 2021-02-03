6ea70eac067ed21ca3d380a5df982d954ab8bb8f
Rémy MEJA <rmeja@issn.org>
2021-02-03 16:36:44 +0100

    add 007 and 008 field in docs

--- src/docs/src/App.vue
+++ src/docs/src/App.vue
@@ -5,6 +5,7 @@
         <h1 class="title">
           PROFILE OF ISSN - MARC 21 / JSON SCHEMA HIMARC (under construction)
         </h1>
+
         <div class="card mb-3">
           <header class="card-header">
             <p class="card-header-title">
@@ -18,52 +19,52 @@
               </span>
             </p>
           </header>
-            <div class="card-content">
-              <div class="content">
-                <table
-                  class="table is-narrow is-striped is-hoverable is-bordered"
-                >
-                  <thead class="has-background-info-light">
-                    <tr>
-                      <th class="has-text-right">Position</th>
-                      <th class="has-text-left">Title</th>
-                      <th>Mandatory field</th>
-                      <th>Details</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr v-for="[position, value] in leaderPositions" :key="position">
-                      <td class="has-text-right">{{ position }}</td>
-                      <td class="has-text-left">{{ value.title }}</td>
-                      <td>
-                        <span class="tag is-warning is-light mx-2" v-if="value.profilISSN">
-                          {{ value.profilISSN }}
-                        </span>
-                      </td>
-                      <td>
-                        <table
-                          class="table is-narrow is-striped is-hoverable is-bordered"
-                          v-if="'codes' in value"
-                        >
-                          <thead class="has-background-info-light">
-                            <tr>
-                              <th class="has-text-right">Code</th>
-                              <th class="has-text-left">Title</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr v-for="(value, code) in value.codes" :key="value.title">
-                              <td class="has-text-right">{{ code }}</td>
-                              <td class="has-text-left">{{ value.title }}</td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+          <div class="card-content">
+            <div class="content">
+              <table
+                class="table is-narrow is-striped is-hoverable is-bordered"
+              >
+                <thead class="has-background-info-light">
+                  <tr>
+                    <th class="has-text-right is-narrow">Position</th>
+                    <th>Title</th>
+                    <th class="has-text-centered">Mandatory field</th>
+                    <th class="has-text-centered">Details</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="[position, value] in leaderPositions" :key="position">
+                    <td>{{ position }}</td>
+                    <td class="has-text-left">{{ value.title }}</td>
+                    <td class="has-text-centered">
+                      <span class="tag is-warning is-light mx-2" v-if="value.profilISSN">
+                        {{ value.profilISSN }}
+                      </span>
+                    </td>
+                    <td>
+                      <table
+                        class="table is-narrow is-striped is-hoverable is-bordered"
+                        v-if="'codes' in value"
+                      >
+                        <thead class="has-background-info-light">
+                          <tr>
+                            <th class="has-text-right is-narrow">Code</th>
+                            <th class="has-text-left">Title</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr v-for="(value, code) in value.codes" :key="value.title">
+                            <td class="has-text-right">{{ code }}</td>
+                            <td class="has-text-left">{{ value.title }}</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
+          </div>
         </div>
 
         <div class="card mb-3" v-for="controlfield in controlfields" :key="controlfield.tag">
@@ -94,6 +95,124 @@
               </span>
             </p>
           </header>
+          <div class="card-content">
+            <div class="content">
+              <table
+                class="table is-narrow is-striped is-hoverable is-bordered"
+                v-for="(field007Category, index) in field007Categories"
+                :key="index"
+              >
+                <thead class="has-background-info-light">
+                  <tr>
+                    <th class="has-text-right is-narrow">Position</th>
+                    <th class="has-text-left">Title</th>
+                    <th>Mandatory field</th>
+                    <th>Details</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(value, position) in field007Category" :key="position">
+                    <td class="has-text-right">{{ position }}</td>
+                    <td class="has-text-left">{{ value.title }}</td>
+                    <td>
+                      <span class="tag is-warning is-light mx-2" v-if="value.profilISSN">
+                        {{ value.profilISSN }}
+                      </span>
+                    </td>
+                    <td>
+                      <table
+                        class="table is-narrow is-striped is-hoverable is-bordered"
+                        v-if="'codes' in value"
+                      >
+                        <thead class="has-background-info-light">
+                          <tr>
+                            <th class="has-text-right is-narrow">Code</th>
+                            <th class="has-text-left">Title</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr v-for="(value, code) in value.codes" :key="value.title">
+                            <td class="has-text-right">{{ code }}</td>
+                            <td class="has-text-left">{{ value.title }}</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
+        <div class="card mb-3">
+          <header class="card-header">
+            <p class="card-header-title">
+              <span class="tag is-primary is-medium mr-2">008</span>
+              {{ field008.title }}
+              <span class="tag is-light mx-2">
+                {{ field008.isRepeatable? 'repeatable' : 'non repeatable' }}
+              </span>
+              <span class="tag is-warning is-light mx-2" v-if="field008.profilISSN">
+                {{ field008.profilISSN }}
+              </span>
+            </p>
+          </header>
+          <div class="card-content">
+            <div class="content">
+              <table
+                class="table is-narrow is-striped is-hoverable is-bordered"
+                v-for="(field008Type, name) in field008Types"
+                :key="name"
+              >
+                <thead class="has-background-info-light">
+                  <tr>
+                    <th colspan="4">
+                      {{ name }}
+                    </th>
+                  </tr>
+                </thead>
+                <thead class="has-background-info-light">
+                  <tr>
+                    <th class="has-text-right is-narrow">Position</th>
+                    <th class="has-text-left">Title</th>
+                    <th class="has-text-centered">Mandatory field</th>
+                    <th>Details</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="[position, value] in getPositionFromField008Type(field008Type)" :key="position">
+                    <td class="has-text-right">{{ position }}</td>
+                    <td class="has-text-left">{{ value.title }}</td>
+                    <td class="has-text-centered">
+                      <span class="tag is-warning is-light mx-2" v-if="value.profilISSN">
+                        {{ value.profilISSN }}
+                      </span>
+                    </td>
+                    <td>
+                      <table
+                        class="table is-narrow is-striped is-hoverable is-bordered"
+                        v-if="'codes' in value"
+                      >
+                        <thead class="has-background-info-light">
+                          <tr>
+                            <th class="has-text-right is-narrow">Code</th>
+                            <th class="has-text-left">Title</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr v-for="(value, code) in value.codes" :key="value.title">
+                            <td class="has-text-right">{{ code }}</td>
+                            <td class="has-text-left">{{ value.title }}</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
 
         <div class="card mb-3" v-for="datafield in datafields" :key="datafield.tag">
@@ -118,13 +237,13 @@
                   >
                     <thead class="has-background-info-light">
                       <tr>
-                        <th class="has-text-right">Indicator 1</th>
+                        <th class="has-text-right is-narrow">Indicator 1</th>
                         <th class="has-text-left">Title</th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr v-for="(value, key) in datafield.value.properties.indicator1.code" :key="key">
-                        <td class="has-text-right">{{ key }}</td>
+                        <td class="has-text-right is-narrow">{{ key }}</td>
                         <td class="has-text-left">{{ value.title }}</td>
                       </tr>
                     </tbody>
@@ -136,7 +255,7 @@
                   >
                     <thead class="has-background-info-light">
                       <tr>
-                        <th class="has-text-right">Indicator 2</th>
+                        <th class="has-text-right is-narrow">Indicator 2</th>
                         <th class="has-text-left">Title</th>
                       </tr>
                     </thead>
@@ -155,22 +274,22 @@
               >
                 <thead class="has-background-info-light">
                   <tr>
-                    <th class="has-text-right">Subfield zone</th>
-                    <th class="has-text-left">Title</th>
-                    <th>Mandatory field</th>
-                    <th>Is Repeatable ?</th>
+                    <th class="has-text-right is-narrow">Subfield zone</th>
+                    <th>Title</th>
+                    <th class="has-text-centered is-narrow">Mandatory field</th>
+                    <th class="has-text-centered is-narrow">Is Repeatable ?</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr v-for="(value, key) in datafield.value.properties.subFields.items.properties" :key="key">
                     <td class="has-text-right">${{ key }}</td>
-                    <td class="has-text-left">{{ value.title }}</td>
-                    <td>
+                    <td>{{ value.title }}</td>
+                    <td class="has-text-centered">
                       <span class="tag is-warning is-light mx-2" v-if="value.profilISSN">
                         {{ value.profilISSN }}
                       </span>
                     </td>
-                    <td>
+                    <td class="has-text-centered">
                       <b-checkbox v-model="value.isRepeatable"></b-checkbox>
                     </td>
                   </tr>
@@ -190,9 +309,6 @@ import schemaRegister from "../../../dist/himarc-register.schema.json";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
   data: function () {
     return {
       isProfilISSN: true
@@ -212,6 +328,16 @@ export default {
     },
     field007: function () {
       return schemaRegister.properties.fields.properties['007'];
+    },
+    field007Categories: function () {
+      return Object.values(schemaRegister.properties.fields.properties['007'].definitions)
+        .map(item => item.properties);
+    },
+    field008: function () {
+      return schemaRegister.properties.fields.properties['008'];
+    },
+    field008Types: function () {
+      return schemaRegister.properties.fields.properties['008'].definitions;
     },
     controlfields: function() {
       return Object.entries(schemaRegister.properties.fields.properties)
@@ -235,6 +361,16 @@ export default {
           return output;
         });
     }
+  },
+  methods: {
+    getPositionFromField008Type: function (field008Type) {
+      return Object.entries(field008Type.properties.positions.properties)
+        .sort(([a], [b]) =>{
+            if (a.length > 2) a = a.slice(0, 2);
+            if (b.length > 2) b = b.slice(0, 2);
+            return a - b;
+          });
+    }
   }
 };
 </script>
@@ -244,8 +380,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
